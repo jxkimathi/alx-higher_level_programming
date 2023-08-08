c736128af66b1c8e40823408cefaee86c5a5550b
jxkimathi <judescriva8@gmail.com>
2023-08-08 19:59:10 +0300

    Inventing is a combination of brains and materials

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+"""Prints two number combinations between 0 and 100"""
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
