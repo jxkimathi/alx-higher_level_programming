4d4270406173fb1ccc148f75340fb928d31e9c17
jxkimathi <judescriva8@gmail.com>
2023-08-08 19:57:52 +0300

    When I was having that alphabet soup

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/bin/bash
+"""Prints the lowercase alphabet minus q and e"""
+for index in range(97, 123):
+    if chr(index) != 'q' and chr(index) != 'e':
+        print("{}".format(chr(index)), end="")
