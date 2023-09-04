a32e783ba8cb852b18e307cc9dcbcac017ff3f4a
jxkimathi <judescriva8@gmail.com>
2023-09-04 21:57:32 +0300

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Prints an empty rectangle"""
+
+
+class Rectangle:
+    """Represents the rectangle"""
+    def __init__(self):
+        """Initializes the rectangle"""
+        pass
