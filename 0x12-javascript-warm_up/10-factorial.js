57bfc8f7ec7a2f49ef03276353a31f702fd44f80
jxkimathi <judescriva8@gmail.com>
2023-11-13 22:34:16 +0300

    corrections

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,7 +6,7 @@ function factorial(a) {
   if ((a === 0) || isNaN(a)) {
     return (1);
   }
-  return (n * factorial(n - 1));
+  return (a * factorial(a - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,9 @@ if (process.argv[2] === undefined || NaN(process.argv[2])) {
   console.log('Missinf number of occurrences');
 } else {
   const x = Number(process.argv[2]);
-
-  for (index = 0; index < x; index++) {
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
