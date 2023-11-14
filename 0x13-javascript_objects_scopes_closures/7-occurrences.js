8c072d5e7393d09c03cca7bf2c3dbd48ea4243cf
jxkimathi <judescriva8@gmail.com>
2023-11-14 18:39:52 +0300

    corrections

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -5,6 +5,6 @@ exports.nbOccurences = function (list, searchElement) {
     if (searchElement === list[i]) {
       nbOccurences++;
     }
-    return (nbOccurences);
-  };
+  }
+  return (nbOccurences);
 };
