b98fb0bf9f0198d8f8a4f270a53029bd5a25b0bf
jxkimathi <judescriva8@gmail.com>
2024-01-10 00:21:01 +0300

    corrections

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-  console.log("My nymber:", parseInt(process.argv[2]));
+  console.log('My nymber:', parseInt(process.argv[2]));
 }
